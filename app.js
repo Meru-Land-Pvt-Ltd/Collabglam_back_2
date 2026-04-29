d881d721db77d12703d5c4ada47c5bb68b73d5d6
namanjha-01 <namanjha@collabglam.com>
2026-04-29 15:29:35 +0530

    changes

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const GRIDFS_BUCKET_NAME = process.env.GRIDFS_BUCKET || "uploads";
 
 const defaultCorsOrigins = [
   "https://collabglam.com",
-  "https://collabglam.cloud",
+  "https://collabglam.cloud/api",
   "http://localhost:3000",
   "http://localhost:3001",
   "http://192.168.1.4:3000",
