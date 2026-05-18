4569954468f0ba6d6097a64142d8cae9486c3a44
Devansh <devanshd78@gmail.com>
2026-05-18 16:08:58 +0530

    updates

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ const defaultCorsOrigins = [
   "http://localhost:4000",
   "http://192.168.1.45:3000",
 
-  "http://192.168.1.23:3000",
+  "http://192.168.1.4:3000",
   "https://mhd.sharemitra.com",
 ];
 
