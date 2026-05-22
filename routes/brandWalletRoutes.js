384167ef2969d9ebf608ef981d86e90892206018
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2026-05-22 18:27:52 +0530

    update

--- routes/brandWalletRoutes.js
+++ routes/brandWalletRoutes.js
@@ -5,12 +5,14 @@ const {
   getBrandWallet,
   topupBrandWallet,
   getFrozenAmountForCampaign,
-  confirmBrandWalletTopup
+  confirmBrandWalletTopup,
+  getWalletTopup,
 } = require("../controllers/brandWalletController");
 
 router.get("/", getBrandWallet);
 router.post("/topup", topupBrandWallet);
 router.post("/topup/confirm", confirmBrandWalletTopup);
 router.get("/freeze-amount", getFrozenAmountForCampaign);
+router.get("/topupHistory", getWalletTopup);
 
 module.exports = router;
